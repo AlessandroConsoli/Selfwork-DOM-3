2675d84355ec5d8d14088070a2b8c43d27f6f569
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-09-13 17:54:40 +0200

    quasi finito

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
-let time = document.querySelector('#input1');
-let customBtn = document.querySelector('custom-Btn');
-let contatore = document.querySelector('contatore');
+// Domanda: Quando inserisco un valore dopo aver fatto reset parte lo stesso dal valore precedente  
 
 
+let countDownInput = document.querySelector('#countDownInput');
+let remainingTime = document.querySelector('#remainingTime');
+let startBtn = document.querySelector('#startBtn');
+let stopBtn = document.querySelector('#stopBtn');
+let resetBtn = document.querySelector('#resetBtn');
 
-customBtn.addEventListener('click', (valore)=>{
-    function countDown() {
-        for (let i = 0; i > 0; i--) {
-            console.log(i);
-            valore.countDown();
-            contatore.innerHTML = i;        
-        }
+let interval;
+let counter;
+let remainingSeconds = 0;
+
+
+// PULSANTI:
+
+// START
+startBtn.addEventListener('click', ()=>{
+    clearInterval(interval);
+    counter = countDownInput.value;    //<=== Valore inserito dall'utente
+
+    if (countDownInput.value > 300) {
+        alert(`Ho un impegno per Natale! \nPer favore inserisci un valore non superiore a 300 sec!`)
+        counter = ``;
+        clearInterval(interval)
+        remainingTime.value = ``;
+        remainingSeconds.innerHTML = '';        
     }
-});
+
+
+    if (remainingSeconds !== 0){
+        counter = remainingSeconds;
+    }
+
+    interval = setInterval( ()=>{
+        if (counter < 0) {
+            clearInterval(interval)
+            remainingTime.innerHTML = `Tempo scaduto`
+        }else{
+            remainingTime.innerHTML = counter
+            counter --
+        }
+    }, 1000);
+})
+
+
+// STOP
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(interval);
+    remainingSeconds = counter;
+})
+
+
+//  RESET
+resetBtn.addEventListener('click', ()=>{
+    countDownInput.value = ``;
+    clearInterval(interval)
+    remainingTime.innerHTML = ``;
+})
+
+
 
 
 
